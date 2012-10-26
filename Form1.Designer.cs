3f527e02407e2b579bde53f3607ec1b1832daae5
Wikzo <Linkthewindwaker@gmail.com>
2012-10-27 01:53:36 +0200

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WordOfTheDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordToShow = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.check)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wordToShow
+            // 
+            this.wordToShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordToShow.Location = new System.Drawing.Point(12, 12);
+            this.wordToShow.Name = "wordToShow";
+            this.wordToShow.Size = new System.Drawing.Size(244, 26);
+            this.wordToShow.TabIndex = 0;
+            this.wordToShow.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // check
+            // 
+            this.check.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.check.Image = global::WordOfTheDay.Properties.Resources.checkmark;
+            this.check.Location = new System.Drawing.Point(155, 44);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(101, 79);
+            this.check.TabIndex = 3;
+            this.check.TabStop = false;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "I have used this word today!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 127);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.wordToShow);
+            this.Name = "Form1";
+            this.Text = "Word of the Day";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.check)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox wordToShow;
+        private System.Windows.Forms.PictureBox check;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordOfTheDay
+{
+
+    public partial class Form1 : Form
+    {
+        private readonly List<string> _availableWordsTotal = new List<string>();
+        private readonly List<string> _learnedWords = new List<string>();
+        private string _currentWord = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckIfTextFilesExist();
+ 
+            GetAllAvailableWords();
+            GetLearnedWords();
+
+            if (GetCurrentWord() != "")
+            {
+                _currentWord = GetCurrentWord();
+                UpdateTextToDisplay();
+            }
+            else
+                UpdateTextToDisplay("No word has ben chosen yet.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void UpdateTextToDisplay()
+        {
+            wordToShow.Text = _currentWord;
+        }
+
+        private void UpdateTextToDisplay(string text)
+        {
+            wordToShow.Text = text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void GetRandomWord()
+        {
+            // TODO: chance of getting an already-learned word
+
+            Random random = new Random(DateTime.Now.Millisecond);
+            int wordCount = _availableWordsTotal.Count();
+            int index;
+            int tries = 0;
+            bool duplicate = false;
+
+            // Pick a word that has not been learned yet
+            index = random.Next(0, wordCount);
+
+            if (_learnedWords.Contains(_availableWordsTotal[index]))
+            {
+                while (tries < 500)
+                {
+                    tries++;
+                    index = random.Next(0, wordCount);
+
+                    if (!_learnedWords.Contains(_availableWordsTotal[index]))
+                    {
+                        duplicate = false;
+                        break;
+                    }
+                    duplicate = true;
+                }
+            }
+
+            if (duplicate == true)
+            {
+                string duplicateString = _availableWordsTotal[index] + "_duplicate";
+                PutInCurrentWord(duplicateString);
+            }
+            else
+                PutInCurrentWord(_availableWordsTotal[index]);
+            
+            wordToShow.Text = _currentWord;
+        }
+
+        private void GetAllAvailableWords()
+        {
+            using (StreamReader reader = new StreamReader("AllWords.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    _availableWordsTotal.Add(line);
+                }
+            }
+        }
+
+        private void GetLearnedWords()
+        {
+            using (StreamReader reader = new StreamReader("LearnedWords.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    _learnedWords.Add(line);
+                }
+            }
+        }
+
+        private void PutInCurrentWord(string word)
+        {
+            using (StreamWriter writer = new StreamWriter("CurrentWord.txt"))
+            {
+                writer.WriteLine(word);
+                _currentWord = word;
+            }
+        }
+
+        string GetCurrentWord()
+        {
+            using (StreamReader reader = new StreamReader("CurrentWord.txt"))
+            {
+                string line;
+                if ((line = reader.ReadLine()) != null)
+                    return line;
+                return "";
+            }
+        }
+
+        void ClearCurrentWordList()
+        {
+            if (_currentWord != "")
+            {
+                using (StreamWriter writer = File.AppendText("LearnedWords.txt"))
+                {
+                    writer.WriteLine(_currentWord);
+                }
+
+                _learnedWords.Add(_currentWord);
+            }
+
+            File.WriteAllText("CurrentWord.txt", String.Empty);
+            _currentWord = "";
+            UpdateTextToDisplay("No word has ben chosen yet.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Sure?", "Confirm", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                ClearCurrentWordList();
+            }
+            
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure that you have used this word enough times?", "Confirm", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                ClearCurrentWordList();
+                GetRandomWord();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckIfTextFilesExist()
+        {
+            if (!File.Exists("AllWords.txt"))
+            {
+                using (StreamWriter writer = File.CreateText("AllWords.txt"))
+                {
+                    writer.WriteLine("Word1");
+                    writer.WriteLine("Word2");
+                    writer.WriteLine("Word3");
+                }
+            }
+
+            if (!File.Exists("CurrentWord.txt"))
+                File.CreateText("CurrentWord.txt");
+
+            if (!File.Exists("LearnedWords.txt"))
+                File.Create("LearnedWords.txt");
+        }
+    }
+}
